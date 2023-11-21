de593c5ff93dd3615e8eb7fd1b4d2fe96b946c1e
Real-Musafir <alshahadath@gmail.com>
2023-11-21 14:44:58 +0600

    brought cars data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import { carsData } from "./utils/carsData";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">okk well come</h1>
+      {carsData.map((item, index) => (
+        <div key={index}>
+          <h1 className="text-black m-5 text-2xl">{item.model}asdf</h1>
+        </div>
+      ))}
     </div>
   );
 }
