084c7447b603880c7bfcd7edd13f109764cbdaea
Real-Musafir <alshahadath@gmail.com>
2023-11-21 15:36:15 +0600

    complete sorted functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { carsData, SORT_TYPE_LIST } from "./utils/carsData";
 function App() {
   const [sortType, setSortType] = useState(null);
+  const [allCarsData, setAllCarsData] = useState(carsData);
+
+  // let sScore = JSON.parse(JSON.stringify(scoreArray))
+
+  // Dynamic sorting function
+  const sortByProperty = (property) => (a, b) => {
+    if (property != "isInProduction") {
+      const propA = a[property].toUpperCase();
+      const propB = b[property].toUpperCase();
+
+      if (propA < propB) {
+        return -1;
+      } else if (propA > propB) {
+        return 1;
+      } else {
+        return 0;
+      }
+    } else {
+      return a[property] - b[property];
+    }
+  };
+
+  useEffect(() => {
+    if (sortType) {
+      let sortedData = carsData.sort(sortByProperty(sortType));
+      setAllCarsData(sortedData);
+    }
+  }, [sortType]);
   return (
     <div className="bg-white flex flex-col items-center">
       {/* Sort Filter Section */}
@@ -29,7 +57,7 @@ function App() {
       </div>
 
       {/* Data Section */}
-      {carsData.map((item, index) => (
+      {allCarsData.map((item, index) => (
         <div key={index}>
           <h1 className="text-red-800 m-5 text-3xl font-bold ">{item.model}</h1>
         </div>
